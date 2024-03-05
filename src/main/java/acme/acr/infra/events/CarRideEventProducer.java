6533e9d8c33232b10c9682c88083cb2dd1cc3309
jbcodeforce <jerome.boyer@gmail.com>
2024-03-05 09:46:56 -0800

    add interface for messaing

--- src/main/java/acme/acr/infra/events/CarRideEventProducer.java
+++ src/main/java/acme/acr/infra/events/CarRideEventProducer.java
@@ -1,54 +1,13 @@
 package acme.acr.infra.events;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.logging.Logger;
-
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.eclipse.microprofile.reactive.messaging.Message;
-
-import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
-
 import acme.acr.domain.CarRide;
-import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
-public class CarRideEventProducer {
-    
-    Logger logger = Logger.getLogger(CarRideEventProducer.class.getName());
-    
-    @Channel("car_rides")
-	public Emitter<CarRideEvent> eventProducer;
+public interface CarRideEventProducer {
 
+    void sendCarRideCreatedEventFrom(CarRide carRide);
 
-    public void sendCarRideCreatedEventFrom(CarRide carRide) {
-        CarRideEvent carRideEvent = new CarRideEvent("RideID_"+carRide.rideId);
-        CarRideCreatedEvent oce = new CarRideCreatedEvent(carRide);
-		carRideEvent.payload = oce;
-        sendCarRideEvent(carRideEvent.eventId,carRideEvent);
-    }
+    void sendCarRideUpdateEventFrom(CarRide carRide);
 
-    public void sendCarRideUpdateEventFrom(CarRide carRide) {
-        CarRideEvent carRideEvent = new CarRideEvent("RideID_"+carRide.rideId);
-        CarRideUpdatedEvent oce = new CarRideUpdatedEvent(carRide);
-		carRideEvent.payload = oce;
-        sendCarRideEvent(carRideEvent.eventId,carRideEvent);
-    }
-     
+    void sendCarRideEvent(String key, CarRideEvent carRideEvent);
 
-    public void sendCarRideEvent(String key, CarRideEvent carRideEvent){
-        logger.info("key " + key + " car ride event " + carRideEvent.eventId
-                    + " type:" + carRideEvent.eventType 
-                    + " ts: " + carRideEvent.timestampMillis);
-
-		eventProducer.send(Message.of(carRideEvent).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
-			.withKey(key).build())
-			.withAck( () -> {
-				
-				return CompletableFuture.completedFuture(null);
-			})
-			.withNack( throwable -> {
-				return CompletableFuture.completedFuture(null);
-			}));
-	}
 }
